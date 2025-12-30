c305f211eca4665e61996e45f503ef25214908d0
thong <thong@purusgames.com>
2025-12-30 14:56:20 +0700

    remove API gg

--- Client/src/lib/firebase.ts
+++ Client/src/lib/firebase.ts
@@ -2,15 +2,16 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyClWQCtIwXAkWunvWkjCK8Y3ZQgYSAgf34",
-  authDomain: "kra-tognoek.firebaseapp.com",
-  projectId: "kra-tognoek",
-  storageBucket: "kra-tognoek.firebasestorage.app",
-  messagingSenderId: "923609492556",
-  appId: "1:923609492556:web:1dd521fb6b4dc131d258e9",
-  measurementId: "G-WD8X7CYB4X"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export default app;
